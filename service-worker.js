4ade08bb622df8d26163cc5a59549982a5a57b02
milkvillage <milk.vllg@gmail.com>
2026-06-02 23:27:21 +0900

    Refresh installed PWA updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "milk-village-pwa-v1";
+const CACHE_NAME = "milk-village-pwa-v2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg"];
 
 self.addEventListener("install", (event) => {
@@ -17,6 +17,10 @@ self.addEventListener("activate", (event) => {
       .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
       .then(() => self.clients.claim()),
   );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
 });
 
 self.addEventListener("fetch", (event) => {
